352b6f1904c31818366282a6f796dd0385c76b17
LamNguyen2608 <phuclam.work@gmail.com>
2021-10-10 17:04:19 +0700

    second js file

--- Node.js
+++ Node.js
@@ -1,24 +1,21 @@
 const http = require("http");
 const hostname = "localhost";
-const port = process.env.PORT || 3000;
 const fs = require("fs");
-const server = http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    switch (req.url){
-        case "/home":
-            res.writeHead(200);
-            var data = fs.readFileSync('./Index.html');
-            res.end(data.toString());
-            break;
-        case "/about":
-            res.writeHead(200);
-            var data1 = fs.readFileSync('./About.html');
-            res.end(data.toString());
-            break;
-        default:
-            res.writeHead(200);
-            res.end("<h1>Easy on me</h1>")
-            break;
-    }
+const path = require("path");
+const express = require("Express");
+//const handlebars = require("express-handlebars");
+const app = express();
+const { append } = require("vary");
+const port = process.env.PORT || 3000;
+
+//image source
+app.use('/images', express.static(__dirname + '/images'));
+
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/About.html');
 });
+app.get('/cart', (req,res) => {
+    res.sendFile(__dirname + '/Cart.html');
+});
+app.listen(port, () => console.log('Listening'));
