a04924bf1009cc5143fc41c6d0a04062ea98a7c0
Lam Nguyen Huu Phuc <86996855+LamNguyen2608@users.noreply.github.com>
2021-10-11 08:55:51 +0700

    Update Node.js

--- Node.js
+++ Node.js
@@ -13,22 +13,51 @@ app.use('/images', express.static(__dirname + '/images'));
 
 
 router.get('/', (req,res) => {
-    res.sendFile(__dirname + '/Index.html');
+    res.statusCode = 200;
+    res.setHeader("Content-Type","text/html");
+    res.writeHead(200);
+    var data = fs.readFileSync("./Index.html");
+    res.end(data.toString());
 });
 router.get('/cart', (req,res) => {
-    res.sendFile(__dirname + '/Cart.html');
+    res.statusCode = 200;
+    res.setHeader("Content-Type","text/html");
+    res.writeHead(200);
+    var data = fs.readFileSync("./Cart.html");
+    res.end(data.toString());
+    //res.sendFile(__dirname + '/Cart.html');
 });
 router.get('/product', (req,res) => {
-    res.sendFile(__dirname + '/Product.html');
+    res.statusCode = 200;
+    res.setHeader("Content-Type","text/html");
+    res.writeHead(200);
+    var data = fs.readFileSync("./Product.html");
+    res.end(data.toString());
+    //res.sendFile(__dirname + '/Product.html');
 });
 router.get('/signup', (req,res) => {
-    res.sendFile(__dirname + '/Signup.html');
+    res.statusCode = 200;
+    res.setHeader("Content-Type","text/html");
+    res.writeHead(200);
+    var data = fs.readFileSync("./Signup.html");
+    res.end(data.toString());
+    //res.sendFile(__dirname + '/Signup.html');
 });
 router.get('/login', (req,res) => {
-    res.sendFile(__dirname + '/User_login.html');
+    res.statusCode = 200;
+    res.setHeader("Content-Type","text/html");
+    res.writeHead(200);
+    var data = fs.readFileSync("./User_login.html");
+    res.end(data.toString());
+    //res.sendFile(__dirname + '/User_login.html');
 });
 router.get('/about', (req,res) => {
-    res.sendFile(__dirname + '/About.html');
+    res.statusCode = 200;
+    res.setHeader("Content-Type","text/html");
+    res.writeHead(200);
+    var data = fs.readFileSync("./About.html");
+    res.end(data.toString());
+    //res.sendFile(__dirname + '/About.html');
 });
 
 app.use("/", router);
